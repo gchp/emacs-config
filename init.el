1f781b9db2267b434f53182be0d561137ec0b42a
Greg Chapple <gregchapple1@gmail.com>
2013-09-02 11:56:49 +0100

    Added auto-completion, expand-region, less-css-mode

--- init.el
+++ init.el
@@ -6,16 +6,19 @@
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-(defvar my-packages '(zenburn-theme
-		      icomplete+
-		      magit))
+(setq user-lib-dir (expand-file-name "lib" user-emacs-directory))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(add-to-list 'load-path user-lib-dir)
+
+(dolist (project (directory-files user-lib-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
 
 ;; load zenburn theme
 (load-theme 'zenburn t)
+
+;; set font
+(set-default-font "Source Code Pro")
 
 ;; hide menu bar and toolbar
 (menu-bar-mode -1)
@@ -33,6 +36,19 @@
 (icomplete-mode 1)
 (setq icomplete-compute-delay 0)
 (require 'icomplete+)
+
+;; ido
+(require 'ido)
+(ido-mode t)
+
+;; Expand region
+(require 'expand-region)
+(global-set-key (kbd "C-@") 'er/expand-region)
+
+;; auto completion
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/lib/auto-complete//ac-dict")
+(ac-config-default)
 
 
 ;; change surrounding quotes
@@ -58,3 +74,24 @@
 ;key bindings
 (global-set-key (kbd "C-c C-s cs \"") 'surround-change-double-quote)
 (global-set-key (kbd "C-c C-s cs '") 'surround-change-single-quote)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-safe-themes (quote ("3ad55e40af9a652de541140ff50d043b7a8c8a3e73e2a649eb808ba077e75792" default)))
+ '(fci-rule-color "#383838")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
