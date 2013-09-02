0beff8d44f850fa21468e60bff9ca82e66ffc1fb
Greg Chapple <gregchapple1@gmail.com>
2013-09-02 13:51:06 +0100

    Extract custom settings to separate file

--- init.el
+++ init.el
@@ -53,6 +53,10 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/lib/auto-complete//ac-dict")
 (ac-config-default)
 
+(column-number-mode t)
+(show-paren-mode t)
+(global-visual-line-mode t)
+(mouse-avoidance-mode 'banish)
 
 ;; change surrounding quotes
 (defun surround-change-surrounding (quote replacement)
@@ -74,24 +78,8 @@
   (save-excursion
     (surround-change-surrounding "'" "\"")))
 
-;; load custom key bindings
+;; load custom keys
 (require 'key-bindings)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-safe-themes (quote ("3ad55e40af9a652de541140ff50d043b7a8c8a3e73e2a649eb808ba077e75792" default)))
- '(fci-rule-color "#383838")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+(require 'custom)
 
-
-(custom-set-faces
- ;; powerline - change text color and bg color
- '(mode-line ((t (:foreground "#F0DFAF" :background "gray22" :box nil))))
- '(mode-line ((t (:foreground "#F0DFAF" :background "gray20" :box nil))))
- )
