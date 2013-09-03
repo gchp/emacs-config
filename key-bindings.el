6e2a12e5486e91332e29eeb43563976e16878b2d
Greg Chapple <gregchapple1@gmail.com>
2013-09-03 10:43:27 +0100

    Fix multiple cursors keybinding

--- key-bindings.el
+++ key-bindings.el
@@ -1,5 +1,5 @@
 ;; Bind C-@ to expand region
-(global-set-key (kbd "C-@") 'er/expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; Bind C-c C-s cs ("|') to toggle surrounding quotes
 (global-set-key (kbd "C-c C-s cs \"") 'surround-change-double-quote)
