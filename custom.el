95d4be4a98a99ad071bbb873fba7b4d7fa0ed9f2
Greg Chapple <gregchapple1@gmail.com>
2013-09-04 16:40:22 +0100

    Update js2 indentation

--- custom.el
+++ custom.el
@@ -6,19 +6,15 @@
  '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-safe-themes (quote ("3ad55e40af9a652de541140ff50d043b7a8c8a3e73e2a649eb808ba077e75792" default)))
  '(fci-rule-color "#383838")
+ '(js2-basic-offset 2)
  '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F")
-				 (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF")
-				 (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F")
-				 (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF")
-				 (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB")
-				 (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
-)
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 
 
 (custom-set-faces
- ;; powerline - change text color and bg color
- '(mode-line ((t (:foreground "#F0DFAF" :background "gray22" :box nil))))
- '(mode-line ((t (:foreground "#F0DFAF" :background "gray20" :box nil))))
- )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:foreground "#F0DFAF" :background "gray22" :box nil)))))
