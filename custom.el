2d40726b6167e518658de3572c9eef06d909e6ed
Greg Chapple <gregchapple1@gmail.com>
2013-10-29 15:50:58 +0000

    Fix modeline colors

--- custom.el
+++ custom.el
@@ -18,4 +18,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:foreground "#F0DFAF" :background "gray22" :box nil)))))
+ )
--- init.el
+++ init.el
@@ -81,6 +81,10 @@
 (require 'powerline)
 (setq powerline-color1 "gray30")
 (setq powerline-color2 "gray45")
+(set-face-attribute 'mode-line nil
+                    :background "gray22"
+                    :foreground "F0DFAF"
+                    :box nil)
 
 ;; snippets
 ;;(require 'yasnippet)
