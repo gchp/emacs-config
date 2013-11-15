da6e15fef70d10ef29f1a950f251e97f9b58471d
Greg Chapple <gregchapple1@gmail.com>
2013-11-15 12:44:07 +0000

    Add util file

---
+++ init-utils.el
@@ -0,0 +1,7 @@
+(defmacro after-load (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
+(provide 'init-utils)
