601c394322091d53d183da1f78d3bfc63fad80e5
Greg Chapple <gregchapple1@gmail.com>
2014-05-26 14:32:47 +0100

    Update indentation

--- custom.el
+++ custom.el
@@ -10,7 +10,9 @@
  '(js2-basic-offset 4)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(web-mode-code-indent-offset 4)
+ '(web-mode-css-indent-offset 4))
 
 
 (custom-set-faces
