6e8698c34cc8812631ea45a0bd8095fe53482d1b
Greg Chapple <gregchapple1@gmail.com>
2015-03-18 15:01:05 +0000

    Disable emacs-powerline, customise modeline

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -11,14 +11,46 @@
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 
-;; emacs powerline
-(require 'powerline)
-(setq powerline-color1 "gray30")
-(setq powerline-color2 "gray45")
-(set-face-attribute 'mode-line nil
-                    :background "gray22"
-                    :foreground "F0DFAF"
-                    :box nil)
+(defun init-powerline ()
+    (require 'powerline)
+    (setq powerline-color1 "gray30")
+    (setq powerline-color2 "gray45")
+    (set-face-attribute 'mode-line nil
+                        :background "gray22"
+                        :foreground "F0DFAF"
+                        :box nil)
+)
+
+(defun init-modeline ()
+  (setq-default mode-line-format
+      (list
+       '(:eval (propertize "%b " 'face 'font-lock-keyword-face 'help-echo (buffer-file-name)))
+
+       "("
+          (propertize "%02l" 'face 'font-lock-type-face) ","
+          (propertize "%02c" 'face 'font-lock-type-face)
+       ") "
+
+       "["
+          (propertize "%p" 'face 'font-lock-constant-face) "/"
+          (propertize "%I" 'face 'font-lock-constant-face)
+       "] "
+
+       "["
+          '(:eval (propertize "%m" 'face 'font-lock-string-face 'help-echo buffer-file-coding-system))
+       "] "
+
+       '(:eval (propertize (format-time-string "%H:%M")
+              'help-echo
+              (concat (format-time-string "%c; ")
+                      (emacs-uptime "Uptime:%hh"))))
+
+        " --"
+        ;; i don't want to see minor-modes; but if you want, uncomment this:
+        ;; minor-mode-alist  ;; list of minor modes
+        "%-" ;; fill with '-'
+  ))
+)
 
 ;; show marker in left frint for lines not in buffer
 (setq indicate-empty-lines t)
@@ -29,21 +61,20 @@
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message t)
 
-(setq evil-default-cursor t)
-
-(column-number-mode t)
-(global-visual-line-mode t)
+;; (column-number-mode t)
+;; (global-visual-line-mode -1)
 
 (defun init-theme ()
-  (load-theme 'zenburn t))
+  (load-theme 'flatland t))
 
 (defun init-fonts ()
-  (set-default-font "Droid Sans Mono-8")
+  (set-frame-font "Droid Sans Mono-8")
   (add-to-list 'default-frame-alist '(font . "Droid Sans Mono-8")))
 
 
 (defun init-gui-customisations ()
   (init-theme)
+  (init-modeline)
   (setq-default line-spacing 2)
   (init-fonts))
 
